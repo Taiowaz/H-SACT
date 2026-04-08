90d3e03dee5fb39777a2d6bba1b7317032c3fc81
Albin <taiowaz@gmail.com>
2026-04-08 20:29:33 +0800

    更新readme以及env.sh文件

--- env.sh
+++ env.sh
@@ -1,13 +1,20 @@
+# filepath: [env.sh](http://_vscodecontentref_/0)
+#!/bin/bash
+set -e  # 任何命令失败都停止脚本
+
 conda create -n hsact python=3.10 -y
-conda activate hsact
-pip install -e .
+conda run -n hsact pip install -e .
+
 # 非5090
-pip install torch==2.1.0 --index-url https://download.pytorch.org/whl/cu121
-pip install torch-sparse torch_geometric torch_scatter -f https://data.pyg.org/whl/torch-2.1.0+cu121.html
-# 5090
-# pip install torch==2.7.0 --index-url https://download.pytorch.org/whl/cu128
-# pip install torch-sparse torch_geometric torch_scatter -f https://data.pyg.org/whl/torch-2.7.0+cu128.html
-pip uninstall numpy
-conda install numpy==1.23.5 -y
-conda install pandas scikit-learn -y
-pip install pytz clint torchmetrics geoopt
+conda run -n hsact pip install torch==2.1.0 --index-url https://download.pytorch.org/whl/cu121
+conda run -n hsact pip install torch-sparse torch_geometric torch_scatter -f https://data.pyg.org/whl/torch-2.1.0+cu121.html
+
+# 5090 (需要时取消注释)
+# conda run -n hsact pip install torch==2.7.0 --index-url https://download.pytorch.org/whl/cu128
+# conda run -n hsact pip install torch-sparse torch_geometric torch_scatter -f https://data.pyg.org/whl/torch-2.7.0+cu128.html
+
+conda run -n hsact pip uninstall numpy -y
+conda run -n hsact conda install numpy==1.23.5 pandas scikit-learn -y
+conda run -n hsact pip install pytz clint torchmetrics geoopt
+
+echo "Installation completed successfully!"
--- scripts/efficiency.sh
+++ scripts/efficiency.sh
@@ -34,21 +34,21 @@ common_args="
 # echo $! > run_log/run_${dataset}.pid
 
 
-dataset="thgl-myket-subset"
-nohup $run_python $run_file \
-    --exper_name ${exper_name} \
-    --dataset ${dataset} \
-    $common_args \
-    --use_gpu 0 \
-    --device 1 > run_log/run_${dataset}.log 2>&1 &
-echo $! > run_log/run_${dataset}.pid
+# dataset="thgl-myket-subset"
+# nohup $run_python $run_file \
+#     --exper_name ${exper_name} \
+#     --dataset ${dataset} \
+#     $common_args \
+#     --use_gpu 0 \
+#     --device 1 > run_log/run_${dataset}.log 2>&1 &
+# echo $! > run_log/run_${dataset}.pid
 
-dataset="thgl-software-subset"
-nohup $run_python $run_file \
-    --exper_name $exper_name \
-    --dataset $dataset \
-    $common_args \
-    --use_gpu 0 \
-    --device 1 > run_log/run_${dataset}.log 2>&1 &
-echo $! > run_log/run_${dataset}.pid
+# dataset="thgl-software-subset"
+# nohup $run_python $run_file \
+#     --exper_name $exper_name \
+#     --dataset $dataset \
+#     $common_args \
+#     --use_gpu 0 \
+#     --device 1 > run_log/run_${dataset}.log 2>&1 &
+# echo $! > run_log/run_${dataset}.pid
 
